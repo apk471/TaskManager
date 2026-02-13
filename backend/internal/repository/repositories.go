a1a261dfaf65eca9435239e0c09680b3c16f3e2c
Ayush Amin <ayush.amin2022@vitstudent.ac.in>
2026-02-13 09:29:12 +0530

    add repo methods

--- backend/internal/repository/repositories.go
+++ backend/internal/repository/repositories.go
@@ -2,8 +2,17 @@ package repository
 
 import "github.com/apk471/go-taskmanager/internal/server"
 
-type Repositories struct{}
+type Repositories struct{
+	ToDo *TodoRepository
+	Comment *CommentRepository
+	Category *CategoryRepository
+
+}
 
 func NewRepositories(s *server.Server) *Repositories {
-	return &Repositories{}
+	return &Repositories{
+		ToDo: NewTodoRepository(s),
+		Comment: NewCommentRepository(s),
+		Category: NewCategoryRepository(s),
+	}
 }
