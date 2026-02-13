f35cee415f670b3813408961ecc97ff9dd26004c
Ayush Amin <ayush.amin2022@vitstudent.ac.in>
2026-02-13 10:04:18 +0530

    add all services

--- backend/internal/service/services.go
+++ backend/internal/service/services.go
@@ -9,13 +9,23 @@ import (
 type Services struct {
 	Auth *AuthService
 	Job  *job.JobService
+	ToDo *TodoService
+	Comment *CommentService
+	Category *CategoryService
 }
 
 func NewServices(s *server.Server, repos *repository.Repositories) (*Services, error) {
 	authService := NewAuthService(s)
+	todoService := NewTodoService(s , repos.ToDo , repos.Category)
+	commentService := NewCommentService(s , repos.Comment , repos.ToDo)
+	categoryService := NewCategoryService(s , repos.Category)
+
 
 	return &Services{
 		Job:  s.Job,
 		Auth: authService,
+		ToDo: todoService,
+		Comment: commentService,
+		Category: categoryService,
 	}, nil
 }
