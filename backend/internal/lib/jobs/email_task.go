e6cf4000c4e4122788ba1c8050b5d7dd10db137c
Ayush Amin <ayush.amin2022@vitstudent.ac.in>
2026-02-16 18:31:23 +0530

    email cron

--- backend/internal/lib/jobs/email_task.go
+++ backend/internal/lib/jobs/email_task.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"time"
 
+	"github.com/apk471/go-taskmanager/internal/model/todo"
+	"github.com/google/uuid"
 	"github.com/hibiken/asynq"
 )
 
@@ -29,4 +31,53 @@ func NewWelcomeEmailTask(to, firstName string) (*asynq.Task, error) {
 		asynq.MaxRetry(3),
 		asynq.Queue("default"),
 		asynq.Timeout(30*time.Second)), nil
+}
+
+type ReminderEmailTask struct {
+	UserID    string    `json:"user_id"`
+	TodoID    uuid.UUID `json:"todo_id"`
+	TodoTitle string    `json:"todo_title"`
+	DueDate   time.Time `json:"due_date"`
+	TaskType  string    `json:"task_type"` // "due_date_reminder" or "overdue_notification"
+}
+
+func EnqueueReminderEmail(client *asynq.Client, task *ReminderEmailTask) error {
+	payload, err := json.Marshal(task)
+	if err != nil {
+		return err
+	}
+
+	asynqTask := asynq.NewTask(TaskReminderEmail, payload,
+		asynq.MaxRetry(3),
+		asynq.Queue("default"),
+		asynq.Timeout(30*time.Second))
+
+	_, err = client.Enqueue(asynqTask)
+	return err
+}
+
+type WeeklyReportEmailTask struct {
+	UserID         string               `json:"user_id"`
+	WeekStart      time.Time            `json:"week_start"`
+	WeekEnd        time.Time            `json:"week_end"`
+	CompletedCount int                  `json:"completed_count"`
+	ActiveCount    int                  `json:"active_count"`
+	OverdueCount   int                  `json:"overdue_count"`
+	CompletedTodos []todo.PopulatedTodo `json:"completed_todos"`
+	OverdueTodos   []todo.PopulatedTodo `json:"overdue_todos"`
+}
+
+func EnqueueWeeklyReportEmail(client *asynq.Client, task *WeeklyReportEmailTask) error {
+	payload, err := json.Marshal(task)
+	if err != nil {
+		return err
+	}
+
+	asynqTask := asynq.NewTask(TaskWeeklyReportEmail, payload,
+		asynq.MaxRetry(3),
+		asynq.Queue("default"),
+		asynq.Timeout(60*time.Second)) // Longer timeout for report generation
+
+	_, err = client.Enqueue(asynqTask)
+	return err
 }
--- backend/internal/model/todo/dto.go
+++ backend/internal/model/todo/dto.go
@@ -110,3 +110,41 @@ func (p *DeleteTodoPayload) Validate() error {
 	validate := validator.New()
 	return validate.Struct(p)
 }
+
+
+// ------------------------------------------------------------
+// Todo Attachment DTOs
+// ------------------------------------------------------------
+
+type UploadTodoAttachmentPayload struct {
+	TodoID uuid.UUID `param:"id" validate:"required,uuid"`
+}
+
+func (p *UploadTodoAttachmentPayload) Validate() error {
+	validate := validator.New()
+	return validate.Struct(p)
+}
+
+// ------------------------------------------------------------
+
+type DeleteTodoAttachmentPayload struct {
+	TodoID       uuid.UUID `param:"id" validate:"required,uuid"`
+	AttachmentID uuid.UUID `param:"attachmentId" validate:"required,uuid"`
+}
+
+func (p *DeleteTodoAttachmentPayload) Validate() error {
+	validate := validator.New()
+	return validate.Struct(p)
+}
+
+// ------------------------------------------------------------
+
+type GetAttachmentPresignedURLPayload struct {
+	TodoID       uuid.UUID `param:"id" validate:"required,uuid"`
+	AttachmentID uuid.UUID `param:"attachmentId" validate:"required,uuid"`
+}
+
+func (p *GetAttachmentPresignedURLPayload) Validate() error {
+	validate := validator.New()
+	return validate.Struct(p)
+}
